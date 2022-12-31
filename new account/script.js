1bd5a52e5cd3899ee55c5761bcb2f178fa191d0d
tawanzinh9 <tiago.bonito44@hotmail.com>
2022-12-31 08:29:55 -0300

    v2

--- new account/script.js
+++ new account/script.js
@@ -9,11 +9,14 @@ btn.addEventListener("click", (e) => {
     if(inputs[0].value.length < 4 || inputs[1].value.length < 4 || inputs[2].value.length < 4 || inputs[3].value.length < 4) {
         msg.textContent = "Something went wrong, please check"
     } else {
+
         msg.classList.add("msg-ok")
         msg.textContent = "Sucess, welcome to this world!! 😊"
+       
+       
         setTimeout(() => {
-            location.href = "./main/index.html"
-        }, 3000)
+            location.href = "../index.html"
+        }, 4000)
        
     }
 })
--- script.js
+++ script.js
@@ -4,10 +4,6 @@ const msg = document.querySelector(".msg")
 const forget = document.querySelector("#forget")
 const link = document.querySelector("#a")
 
-function setError(err) {
-    inputs[err].style.border = "2px solid #e363636 ";
-}
-
 
 login.addEventListener("click", (e) => {
     e.preventDefault()
@@ -16,10 +12,15 @@ login.addEventListener("click", (e) => {
       msg.classList.remove("msg-ok")
       msg.textContent = "Something seems wrong, please try again."
       msg.classList.add("msg")
+      alert("Remember that your email and password need to be more than 4 digits")
+      
+
+      
     } else {
         msg.classList.remove("msg")
         msg.classList.add("msg-ok")
         msg.textContent = "Success, please, await..."
+        
 
         setTimeout ( () => {
             location.href = "./main/index.html"
